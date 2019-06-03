e4482bc9f929b75a101edb88c85fd08e38e015d8
manasyebukit <13516122@std.stei.itb.ac.id>
2019-06-03 16:19:44 +0700

    Add post list and home

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      id="1"
+      thumbnail="https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg"
+      title="Hello"
+      previewText="preview"
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/about/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about-page">
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
+      aspernatur commodi deserunt dignissimos doloribus ex explicabo fugit ipsum
+      minus modi nemo quam quas, quia quod ratione rem sed tempore unde.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
+      aspernatur commodi deserunt dignissimos doloribus ex explicabo fugit ipsum
+      minus modi nemo quam quas, quia quod ratione rem sed tempore unde.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
+      aspernatur commodi deserunt dignissimos doloribus ex explicabo fugit ipsum
+      minus modi nemo quam quas, quia quod ratione rem sed tempore unde.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
+      aspernatur commodi deserunt dignissimos doloribus ex explicabo fugit ipsum
+      minus modi nemo quam quas, quia quod ratione rem sed tempore unde.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
+      aspernatur commodi deserunt dignissimos doloribus ex explicabo fugit ipsum
+      minus modi nemo quam quas, quia quod ratione rem sed tempore unde.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
